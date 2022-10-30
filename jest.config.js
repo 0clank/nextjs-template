384336b9da5a27db1555c017c96e9a137175ce1c
Danny <dann.becker1996@gmail.com>
2022-10-31 00:06:10 +0100

    Updated workflow

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // on node 14.x coverage provider v8 offers good speed and more or less good report
     coverageProvider: 'v8',
     collectCoverageFrom: [
-        '**/*.{js,jsx,ts,tsx}',
+        './src/**/*.{js,jsx,ts,tsx}',
         '!**/*.d.ts',
         '!**/node_modules/**',
         '!<rootDir>/out/**',
